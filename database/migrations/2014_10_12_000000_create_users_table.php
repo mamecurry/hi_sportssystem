9a73a50d15dfe5d3176ebe1d873c1792d78b3be0
mamecurry <roddyroddyroddyroddy@gmail.com>
2024-09-16 22:48:01 +0900

    migrationを修正2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id');
+            $table->string('group_name');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->string('address');
             $table->rememberToken();
             $table->boolean('is_admin')->nullable();
             $table->timestamps();
