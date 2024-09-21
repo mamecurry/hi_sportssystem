434f8c27da5233f7efb2b3c110466b2b483c2477
kei-37 <tryandconfirm@gmail.com>
2024-09-22 00:44:21 +0900

    dashboard復元

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use SebastianBergmann\Diff\Output\DiffOnlyOutputBuilder;
 
 Route::get('/', [ReservationController::class, 'initial'])->name('reservations.initial');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
