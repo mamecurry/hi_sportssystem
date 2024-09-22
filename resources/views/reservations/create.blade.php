3047ab8e2f13232a6e274fb722340ffd827b0b58
kei-37 <tryandconfirm@gmail.com>
2024-09-22 17:35:01 +0900

    予約登録機能途中

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -25,76 +25,76 @@
                     <div class="mt-0 col-span-2">
                         <div class="grid grid-cols-5">
                             <div class="col-span-2">
-                                <input type="datetime-local" name="reservation_datetime" class="w-full">
+                                <input type="date" name="reservation_datetime" class="w-full">
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <!-- 時間帯リスト -->
-        <div class="mt-10">
-            <h3>{{ __('予約可能な時間帯') }}</h3>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="9:00~10:00">
-                    9:00~10:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="10:00~11:00">
-                    10:00~11:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="11:00~12:00">
-                    12:00~13:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="13:00~14:00">
-                    13:00~14:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="14:00~15:00">
-                    14:00~15:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="15:00~16:00">
-                    15:00~16:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="17:00~18:00">
-                    17:00~18:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="18:00~19:00">
-                    18:00~19:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="19:00~20:00">
-                    19:00~20:00
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" name="time_slots[]" value="20:00~21:00">
-                    20:00~21:00
-                </label>
-            </div>
-        </div>
+                <div class="mt-10">
+                    <h3>{{ __('予約可能な時間帯') }}</h3>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="9:00~10:00">
+                            9:00~10:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="10:00~11:00">
+                            10:00~11:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="11:00~12:00">
+                            12:00~13:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="13:00~14:00">
+                            13:00~14:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="14:00~15:00">
+                            14:00~15:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="15:00~16:00">
+                            15:00~16:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="17:00~18:00">
+                            17:00~18:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="18:00~19:00">
+                            18:00~19:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="19:00~20:00">
+                            19:00~20:00
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="time_slots[]" value="20:00~21:00">
+                            20:00~21:00
+                        </label>
+                    </div>
+                </div>
 
                 <div class="mt-10">
                     <input type="submit" value="{{ __('確認') }}" class="border border-black px-3 py-1">
